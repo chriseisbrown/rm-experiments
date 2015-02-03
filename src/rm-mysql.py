37e77ab3acf437101a34d1ffe26a674784138118
Chris Brown <chriseisbrown@gmail.com>
2015-02-03 13:56:50 +0000

    new mysql versions

--- src/rm-mysql.py
+++ src/rm-mysql.py
@@ -4,6 +4,10 @@ Created on 1 Feb 2015
 @author: chriseisbrown
 '''
 import mysql.connector
+from Article import Article
+
+ABSTRACT_TABLE = "raremark.article_abstract"
+ARTICLE_TABLE = "raremark.article"
 
 cnx = mysql.connector.connect(user='root', database='raremark')
 #cnx = mysql.connector.connect(user="myuser", host='localhost', database='raremark')
@@ -11,11 +15,33 @@ cnx = mysql.connector.connect(user='root', database='raremark')
 cursor = cnx.cursor()
 #query = ('show tables')
 #query=("GRANT ALL ON raremark.* TO 'myuser'@'localhost'")
+
+
+article = Article()
+article._id ="996"
+article.URL = "http://www.ncbi.nlm.nih.gov/pubmed/25385939"
+article.id_type = "PMID"
+article.title = "something with include's apostrophe"
+article.version = 1
+article.doc_version = "1.2"
+article.abstract_text = "some abstract nonsense's"
+article.journal = "journal of something"
+article.publish_date = "2014-01-03"
+
+insert_article_query = "INSERT INTO {} values(%s,%s,%s,%s,%s,%s,%s,%s)".format(ARTICLE_TABLE)
+cursor.execute(insert_article_query, (article._id, article.URL, article.id_type, article.title,
+                           article.version, article.doc_version, article.journal, article.publish_date))
+
+insert_abstract_query = "INSERT INTO {} values(%s, %s)".format(ABSTRACT_TABLE)
+cursor.execute(insert_abstract_query, (article._id, article.abstract_text))             
+                            
+cnx.commit()
+
 query=("SELECT * FROM raremark.article")
-
-
-
 cursor.execute(query)
-
 for row in cursor:
     print row
+
+
+cursor.close()
+cnx.close()
---
+++ src/rm_pull_mysql.py
@@ -0,0 +1,165 @@
+'''
+Created on 22 Jan 2015
+Experiment to pull data from Pubmed and do some simple manipulation on it.
+
+@author: chriseisbrown
+'''
+import os
+import re
+import json
+import requests
+import xml.sax.saxutils as saxutils
+import mysql.connector
+
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+    
+from Article import Article
+from datetime import datetime
+from StringIO import  StringIO
+
+from xlrd import open_workbook
+
+INPUT_FOLDER = "../input-data/"
+INPUT_FILENAME = "publications-summary.xlsx"
+ABSTRACT_TABLE = "raremark.article_abstract"
+ARTICLE_TABLE = "raremark.article"
+
+#http://www.ncbi.nlm.nih.gov/pubmed?term=%22Fabry+Disease%22%5BMesh%5D
+#http://www.ncbi.nlm.nih.gov/pubmed?term=(%22Fabry%20Disease%22%5BMesh%5D)%20AND%20(%22Fabry%20Disease%22%5BMesh%5D%20AND%20(%20%222014%2F01%2F01%22%5BPDat%5D%20%3A%20%222014%2F12%2F31%22%5BPDat%5D%20))
+ 
+
+def main():
+    cnx = mysql.connector.connect(user='root', database='raremark')
+    
+    in_folder_name = INPUT_FOLDER
+    in_filename = INPUT_FILENAME
+    
+    infile = os.path.join(in_folder_name, in_filename)    
+    print "using input data from file {}".format(infile)
+    
+    # make publications map 
+    wb = open_workbook(infile) 
+    article_sheet = wb.sheet_by_name('2014')
+    #article_ids = article_sheet.col_values(0,1)
+    article_urls = article_sheet.col_values(1,1)
+    
+    articles_map = {}
+        
+    for article_url in article_urls:  
+        
+        article = Article()        
+        article.URL = article_url + "?report=xml&format=xml"
+    
+        #url = "http://www.ncbi.nlm.nih.gov/pubmed/25345090?report=xml&format=xml"
+        results = requests.get( article.URL)
+        assert results.status_code == 200
+    
+        raw_txt = results.content
+        
+        # need to unescape data that was returned
+        txt = saxutils.unescape(raw_txt)
+        
+        root = ET.fromstring(txt)
+    
+        for elem in root.iterfind('PubmedArticle/MedlineCitation/PMID'):
+            attributes = elem.attrib
+            article.version = int(attributes['Version'])
+            article.doc_version = attributes['Version']
+            article._id = elem.text
+            print article._id
+            article.id_type = "PMID"
+            
+        for elem in root.iterfind('PubmedArticle/MedlineCitation/Article/ArticleTitle'):
+            article.title = elem.text
+                 
+        for elem in root.iterfind('PubmedArticle/MedlineCitation/Article/Abstract/AbstractText'):
+            article.abstract_text = elem.text 
+            
+        for elem in root.iterfind('PubmedArticle/MedlineCitation/Article/Journal/Title'):
+            article.journal = elem.text 
+            
+        for pub_date in root.iterfind('PubmedArticle/MedlineCitation/Article/Journal/JournalIssue/PubDate'):
+            
+            medline_date = pub_date.find('MedlineDate')
+            if medline_date != None:
+                print "found medline date !"
+                
+                medline_date_txt = medline_date.text
+                
+                # have we a half-decent format ?
+                p1 = re.compile('^\d{4} ([A-Z][a-z]{2}-[A-Z][a-z]{2})')
+                m1 = p1.match(medline_date_txt)
+                # TODO: default this for now and sort out later
+                if m1 is not None:
+                    month_span = m1.group(1)
+                    month = "Apr"
+                else:
+                    month = "Dec"
+                
+                p2 = re.compile('^\d{4}')
+                m2 = p2.match(medline_date_txt)
+                year = m2.group()
+                if year is None:
+                    year = "1900"
+                    
+                day = "01"
+                
+            else:
+                year = pub_date.find('Year')
+                if year is None:
+                    year = "1900"
+                else:
+                    year = year.text
+                
+                month = pub_date.find('Month')
+                if month is None:
+                    month = "Jan"
+                else:
+                    month = month.text
+                  
+                day = pub_date.find('Day')
+                if day is None:
+                    day = "01"
+                else:
+                    day = day.text
+                  
+            published_date = datetime.strptime(year + ' ' + month + ' ' + day, '%Y %b %d' ).date()
+            article.publish_date = published_date.isoformat()  
+            
+        #article.display()    
+        articles_map[article._id] = article
+        
+        
+    # do writes to db from map
+    db_count = 0
+    db_error_count = 0 
+    for article in articles_map.itervalues():    
+        
+        insert_article_query = "INSERT INTO {} values(%s,%s,%s,%s,%s,%s,%s,%s)".format(ARTICLE_TABLE)
+        insert_abstract_query = "INSERT INTO {} values(%s, %s)".format(ABSTRACT_TABLE)
+        
+        try:
+            cursor = cnx.cursor()
+            cursor.execute(insert_article_query, (article._id, article.URL, article.id_type, article.title.encode('utf-8'),
+                               article.version, article.doc_version, article.journal.encode('utf-8'), article.publish_date))
+            cursor.execute(insert_abstract_query, (article._id, article.abstract_text.encode('utf-8')))   
+            cnx.commit()
+            db_count += 1
+        except mysql.connector.Error as error:
+            print "Error {} attempting to insert article {}".format(error, article._id)
+            db_error_count += 1
+            
+        
+    
+    cursor.close()
+    cnx.close()
+    print "Refreshed database with {} items and {} errors".format(db_count, db_error_count)
+
+
+
+if __name__ == "__main__":
+    main()    
+
