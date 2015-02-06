37402b8c66a1d1c86843d146c3e71844b96004dc
Chris Brown <chriseisbrown@gmail.com>
2015-02-06 09:36:16 +0000

    Add new disease candidates

--- raremark_scripts.sql
+++ raremark_scripts.sql
@@ -23,15 +23,29 @@ FROM article AS a
 WHERE a._id=23350580;
 
 commit;
+
 INSERT INTO raremark.disease values(1, "Fabry's disease", "Fabry");
 INSERT INTO raremark.disease values(2, "Huntingdon's disease", "Huntingdon");
 INSERT INTO raremark.disease values(3, "Muscular Atrophy, Spinal", "SMA");
+INSERT INTO raremark.disease values(4, "Duchenne muscular dystrophy", "Duchenne");
+INSERT INTO raremark.disease values(5, "Behçet disease", "Behçet");
+INSERT INTO raremark.disease values(6, "Gaucher disease", "Gaucher");
+INSERT INTO raremark.disease values(7, "Myelofibrosis", "Myelofibrosis");
+
+Proximal spinal muscular atrophy
+
+
+
+
 INSERT INTO raremark.mesh_term values(1, 1, "Anderson-Fabry Disease");
 INSERT INTO raremark.mesh_term values(2, 1, "alpha-Galactosidase A Deficiency Disease");
 INSERT INTO raremark.mesh_term values(3, 1, "Fabry Disease");
 INSERT INTO raremark.mesh_term values(5, 1, "Fabry's disease");
-INSERT INTO raremark.mesh_term values(4, 3, "Muscular Atrophy, Spinal");
+
 INSERT INTO raremark.mesh_term values(6, 2, "Huntington's Disease");
+
+INSERT INTO raremark.mesh_term values(4, 3, "Muscular Atrophy, Spinal");
+
 
 
 INSERT INTO raremark.article(_id,disease,URL,id_type,title,version,doc_version,journal,publish_date) values(
@@ -41,6 +55,5 @@ INSERT INTO raremark.article(_id,disease,URL,id_type,title,version,doc_version,j
 'PMID',
 'Hypothalamic-pituitary-adrenal axis functioning in Huntington\'s disease and its association with depressive symptoms and suicidality.',1,'1','Journal of neuroendocrinology',
 '2015-01-06') 
-
-
-ON DUPLICATE KEY UPDATE             _id =VALUES(_id),disease=(disease),URL =VALUES(URL),id_type =VALUES(id_type),title =VALUES(title),version =VALUES(version),             doc_version =VALUES(doc_version),journal =VALUES(journal),publish_date =VALUES(publish_date)
+ON DUPLICATE KEY UPDATE 
+_id =VALUES(_id),disease=(disease),URL =VALUES(URL),id_type =VALUES(id_type),title =VALUES(title),version =VALUES(version),             doc_version =VALUES(doc_version),journal =VALUES(journal),publish_date =VALUES(publish_date)
